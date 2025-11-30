dad0dbe27057208cf5972e142e9e49cb2c1d41ef
Khalid Bakhit <khalid@Khalids-MacBook-Pro.local>
2025-11-30 01:14:17 -0500

    Fix fc26_bot import for Docker/gunicorn

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, render_template
-from fc26_bot import answer_question
+from .fc26_bot import answer_question
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
